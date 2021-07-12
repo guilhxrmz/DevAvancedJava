e2a4457e18cad6ac0ff1c83eb5e29d1afc74c41b
Guilherme Lourenço <guilhermelourenco132@gmail.com>
2021-07-12 13:42:50 -0300

    add descrition and working also with numbers

--- src/Lambda/FilterName.java
+++ src/Lambda/FilterName.java
@@ -7,10 +7,22 @@ public class FilterName {
     public static  void  main(String[] args){
         String [] nomes = {"maria","vania","duda","alex","mauro","tagashi"};
 
-        var filtrado = Stream.of(nomes).
+        //printa o nome x
+        var filterprint = Stream.of(nomes).
                 filter(nome -> nome.equals("maria"))
                 .collect(Collectors.joining());
+        System.out.println(filterprint);
 
-        System.out.println(filtrado);
+        //printa tudo
+        Stream.of(nomes)
+                .forEach(System.out::println);
+
+        //trabalhando com numeros
+        Integer[] numeros = {2,4,6,8,10};
+
+        //multiplicando por 2
+        Stream.of(numeros).
+                map(numero -> numero*2).
+                forEach(System.out::println);
     }
 }
